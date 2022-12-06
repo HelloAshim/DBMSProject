348174ba10db6435cffbeea335a1450f5e82f3f2
Mohsina Tabassum <mohsinatabassum12@gmail.com>
2022-12-07 01:14:35 +0600

    tabia

---
+++ core/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from core.forms import DepartmentForm, SchoolForm
+from core.models import Department, School
+
+def index(request):
+    return render(request, 'home.html')
+
+def school_list(request):
+    schools=School.objects.all()
+    context={
+        "schools":schools
+    }
+
+    return render(request,'school_list.html',context)
+
+def create_school(request):
+    form = SchoolForm(request.POST or None)
+
+    if(form.is_valid()):
+        form.save()
+        return HttpResponseRedirect(reverse('core:school_list'))
+    context={
+        "form":form,
+    }
+    return render(request, 'create_school.html',context)
